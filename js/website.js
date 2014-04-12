426b3a6f0b9c7287ba7afa2b42a793a1c18479c3
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-04-12 01:51:10 -0400

    my personal website

---
+++ js/website.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  var clearHeader = $('header').outerHeight();
+
+  $(window).scroll(function() {
+    if ( $(window).scrollTop() > clearHeader ) {
+      $('nav').addClass('fixed').css('top','0').next()
+      .css('padding-top','60px');   
+    } else {
+      $('nav').removeClass('fixed').next()
+      .css('padding-top','0');
+    }
+  });
+});
