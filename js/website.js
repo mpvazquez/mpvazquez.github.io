d49dcde3b12667d64b0e595bc5c6a699a624bd08
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-04-30 14:44:48 -0400

    Updated JS to make sticky nav less jumpy

--- js/website.js
+++ js/website.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   $(window).scroll(function() {
     if ( $(window).scrollTop() > clearHeader ) {
       $('nav').addClass('fixed').css('top','0').next()
-      .css('padding-top','60px');   
+      .css('padding-top','50px');   
     } else {
       $('nav').removeClass('fixed').next()
       .css('padding-top','0');
