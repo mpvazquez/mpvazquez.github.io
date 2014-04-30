b6fa3d981193b5351e6be183af6d6a346d4c2a6e
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-04-30 15:47:48 -0400

    added smooth scrolling with javascript/jquery

--- js/website.js
+++ js/website.js
@@ -10,4 +10,18 @@ $(document).ready(function() {
       .css('padding-top','0');
     }
   });
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      console.log(target)
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 20
+        }, "slow");
+        return false;
+      }
+    }
+  });
 });
