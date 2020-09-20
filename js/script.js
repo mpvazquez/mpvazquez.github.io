6cb69e0e4deef3b10fe07887d47ca5b08d203b3a
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2020-09-20 14:34:25 -0400

    Redirect http site to https

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 (function() {
     'use strict';
 
+    var SECURE_PROTOCOL = 'https:';
+    var UNSECURE_PROTOCOL = 'http:';
+
+    if (location.protocol === UNSECURE_PROTOCOL) {
+      location.replace(SECURE_PROTOCOL + location.hostname);
+    }
+
     function animatedScroll(event) {
         var hash = event.currentTarget.hash.slice(1);
 
