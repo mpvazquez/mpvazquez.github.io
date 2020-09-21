8e529ae3070be0c41a170411b8e3fe412f1449fe
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2020-09-21 02:44:24 -0400

    Temp Change form back to default action method submission

--- js/script.js
+++ js/script.js
@@ -22,31 +22,31 @@
         return false;
     }
 
-    contactForm.addEventListener('submit', function(event) {
-      event.preventDefault();
-      var data = {};
-      var formData = new FormData(event.currentTarget);
-      var request = new XMLHttpRequest();
-      var url = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSda5h6UlLjP7DiMPvDMFIF4ZpgA1OY3TbwMEwsYZHao9wF7Zg/formResponse?embedded=true';
-
-      data['entry.633803438'] = formData.get('entry.633803438');
-      data['entry.610708785'] = formData.get('entry.610708785');
-      data['entry.1719665273'] = formData.get('entry.1719665273');
-      data['entry.1633536652'] = formData.get('entry.1633536652');
-
-      request.addEventListener('load', function(success) {
-        console.log('Success submitting form: ', success);
-        contactForm.reset();
-        alert('Thank you for contacting me. Your message has successfully been sent!');
-      });
-      request.addEventListener('error', function(error) {
-        console.error('Error submiting form: ', error);
-      });
-
-      request.open('POST', url, true);
-      request.setRequestHeader('Access-Control-Allow-Origin', '*');
-      request.send(formData);
-    });
+    // contactForm.addEventListener('submit', function(event) {
+    //   event.preventDefault();
+    //   var data = {};
+    //   var formData = new FormData(event.currentTarget);
+    //   var request = new XMLHttpRequest();
+    //   var url = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSda5h6UlLjP7DiMPvDMFIF4ZpgA1OY3TbwMEwsYZHao9wF7Zg/formResponse?embedded=true';
+    //
+    //   data['entry.633803438'] = formData.get('entry.633803438');
+    //   data['entry.610708785'] = formData.get('entry.610708785');
+    //   data['entry.1719665273'] = formData.get('entry.1719665273');
+    //   data['entry.1633536652'] = formData.get('entry.1633536652');
+    //
+    //   request.addEventListener('load', function(success) {
+    //     console.log('Success submitting form: ', success);
+    //     contactForm.reset();
+    //     alert('Thank you for contacting me. Your message has successfully been sent!');
+    //   });
+    //   request.addEventListener('error', function(error) {
+    //     console.error('Error submiting form: ', error);
+    //   });
+    //
+    //   request.open('POST', url, true);
+    //   request.setRequestHeader('Access-Control-Allow-Origin', '*');
+    //   request.send(formData);
+    // });
 
     window.addEventListener('DOMContentLoaded', function() {
         document.querySelectorAll('.nav-link a').forEach(function(navLink) {
