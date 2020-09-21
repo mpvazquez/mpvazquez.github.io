2b5ccf8a2db8a95169cb81685dd1a43d86cb2781
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2020-09-21 02:26:12 -0400

    Fix form submission using JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 (function() {
     'use strict';
 
+    var contactForm = document.getElementById('contact-form');
+
     function animatedScroll(event) {
         var hash = event.currentTarget.hash.slice(1);
 
@@ -20,30 +22,29 @@
         return false;
     }
 
-    // document.getElementById('contact-form').addEventListener('submit', function(event) {
-    //   event.preventDefault();
-    //   var data = {};
-    //   var formData = new FormData(event.currentTarget);
-    //   var request = new XMLHttpRequest();
-    //   var url = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSda5h6UlLjP7DiMPvDMFIF4ZpgA1OY3TbwMEwsYZHao9wF7Zg/formResponse?embedded=true';
-    //
-    //   data['entry.633803438'] = formData.get('entry.633803438');
-    //   data['entry.610708785'] = formData.get('entry.610708785');
-    //   data['entry.1719665273'] = formData.get('entry.1719665273');
-    //   data['entry.1633536652'] = formData.get('entry.1633536652');
-    //
-    //   request.addEventListener('load', function(success) {
-    //     console.log(success);
-    //   });
-    //   request.addEventListener('error', function(error) {
-    //     console.error(error);
-    //   });
-    //
-    //   request.open('POST', url, true);
-    //   request.setRequestHeader('Content-Type', 'multipart/form-data; charset=UTF-8');
-    //   request.setRequestHeader('Access-Control-Allow-Origin', '*');
-    //   request.send(data);
-    // });
+    contactForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+      var data = {};
+      var formData = new FormData(event.currentTarget);
+      var request = new XMLHttpRequest();
+      var url = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSda5h6UlLjP7DiMPvDMFIF4ZpgA1OY3TbwMEwsYZHao9wF7Zg/formResponse?embedded=true';
+
+      data['entry.633803438'] = formData.get('entry.633803438');
+      data['entry.610708785'] = formData.get('entry.610708785');
+      data['entry.1719665273'] = formData.get('entry.1719665273');
+      data['entry.1633536652'] = formData.get('entry.1633536652');
+
+      request.addEventListener('load', function(success) {
+        console.log('Success submitting form: ', success);
+        contactForm.reset();
+      });
+      request.addEventListener('error', function(error) {
+        console.error('Error submiting form: ', error);
+      });
+
+      request.open('POST', url, true);
+      request.send(formData);
+    });
 
     window.addEventListener('DOMContentLoaded', function() {
         document.querySelectorAll('.nav-link a').forEach(function(navLink) {
