2a1d56f8fe30f9a39b9ac07821d175a1a83628ef
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2020-09-21 01:13:01 -0400

    Temp Change form back to default action method submission

--- js/script.js
+++ js/script.js
@@ -20,30 +20,30 @@
         return false;
     }
 
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-      event.preventDefault();
-      var data = {};
-      var formData = new FormData(event.currentTarget);
-      var request = new XMLHttpRequest();
-      var url = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSda5h6UlLjP7DiMPvDMFIF4ZpgA1OY3TbwMEwsYZHao9wF7Zg/formResponse?embedded=true';
-
-      data['entry.633803438'] = formData.get('purpose-field');
-      data['entry.610708785'] = formData.get('name-field');
-      data['entry.1719665273'] = formData.get('email-field');
-      data['entry.1633536652'] = formData.get('message-field');
-
-      request.addEventListener('load', function(success) {
-        console.log(success);
-      });
-      request.addEventListener('error', function(error) {
-        console.error(error);
-      });
-      
-      request.open('POST', url, true);
-      request.setRequestHeader('Content-Type', 'multipart/form-data; charset=UTF-8');
-      request.setRequestHeader('Access-Control-Allow-Origin', '*');
-      request.send(data);
-    });
+    // document.getElementById('contact-form').addEventListener('submit', function(event) {
+    //   event.preventDefault();
+    //   var data = {};
+    //   var formData = new FormData(event.currentTarget);
+    //   var request = new XMLHttpRequest();
+    //   var url = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSda5h6UlLjP7DiMPvDMFIF4ZpgA1OY3TbwMEwsYZHao9wF7Zg/formResponse?embedded=true';
+    //
+    //   data['entry.633803438'] = formData.get('entry.633803438');
+    //   data['entry.610708785'] = formData.get('entry.610708785');
+    //   data['entry.1719665273'] = formData.get('entry.1719665273');
+    //   data['entry.1633536652'] = formData.get('entry.1633536652');
+    //
+    //   request.addEventListener('load', function(success) {
+    //     console.log(success);
+    //   });
+    //   request.addEventListener('error', function(error) {
+    //     console.error(error);
+    //   });
+    //
+    //   request.open('POST', url, true);
+    //   request.setRequestHeader('Content-Type', 'multipart/form-data; charset=UTF-8');
+    //   request.setRequestHeader('Access-Control-Allow-Origin', '*');
+    //   request.send(data);
+    // });
 
     window.addEventListener('DOMContentLoaded', function() {
         document.querySelectorAll('.nav-link a').forEach(function(navLink) {
