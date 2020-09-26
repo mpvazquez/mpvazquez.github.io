4150e8dc1ab2e9f047959a360e4ec8d4cda63b69
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2020-09-26 15:19:18 -0400

    Remove unneeded JS

--- js/script.js
+++ js/script.js
@@ -56,55 +56,13 @@
     }
 
     // Temp testing script for confirmationModal
-    window.go = openModal; // temp
-    openModal();
+    // window.go = openModal; // temp
+    // openModal();
 
     document.getElementById('iframe').addEventListener('load', function(event) {
       contactForm.reset();
       openModal();
     });
-
-    // document.getElementById('iframe').addEventListener('load', function(event) {
-    //   event.preventDefault();
-    //   console.log('event loaded', event);
-    // });
-    //
-    // contactForm.addEventListener('submit', function(event) {
-    //   if ('fetch' in window) {
-    //     event.preventDefault();
-    //
-    //     var data = {};
-    //     var formData = new FormData(event.currentTarget);
-    //     var request = window.fetch();
-    //   }
-    // });
-    //
-    // contactForm.addEventListener('submit', function(event) {
-    //   event.preventDefault();
-    //   var data = {};
-    //   var formData = new FormData(event.currentTarget);
-    //   var request = new XMLHttpRequest();
-    //   var url = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSda5h6UlLjP7DiMPvDMFIF4ZpgA1OY3TbwMEwsYZHao9wF7Zg/formResponse?embedded=true';
-    //
-    //   data['entry.633803438'] = formData.get('entry.633803438');
-    //   data['entry.610708785'] = formData.get('entry.610708785');
-    //   data['entry.1719665273'] = formData.get('entry.1719665273');
-    //   data['entry.1633536652'] = formData.get('entry.1633536652');
-    //
-    //   request.addEventListener('load', function(success) {
-    //     console.log('Success submitting form: ', success);
-    //     contactForm.reset();
-    //     alert('Thank you for contacting me. Your message has successfully been sent!');
-    //   });
-    //   request.addEventListener('error', function(error) {
-    //     console.error('Error submiting form: ', error);
-    //   });
-    //
-    //   request.open('POST', url, true);
-    //   request.setRequestHeader('Access-Control-Allow-Origin', '*');
-    //   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    //   request.send(formData);
-    // });
 
     window.addEventListener('DOMContentLoaded', function() {
         document.querySelectorAll('.nav-link a').forEach(function(navLink) {
