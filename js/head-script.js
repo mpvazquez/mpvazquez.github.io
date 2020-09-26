3c4af556981a673b400888dc72863a8ac7e1f49b
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2020-09-26 16:15:49 -0400

    Add Google Analytics

--- js/head-script.js
+++ js/head-script.js
@@ -7,4 +7,14 @@
     if (location.protocol === UNSECURE_PROTOCOL) {
       location.replace(SECURE_PROTOCOL + location.hostname);
     }
+
+    // Global site tag (gtag.js) - Google Analytics
+    window.dataLayer = window.dataLayer || [];
+
+	  function gtag() {
+			dataLayer.push(arguments);
+		}
+
+	  gtag('js', new Date());
+	  gtag('config', 'UA-21024248-2');
 })();
